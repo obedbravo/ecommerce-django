df84452f90a729a22fce0efbf7b4b9840fd11631
obedbravo <obed.bravo@outlook.com>
2023-09-06 11:05:16 -0700

    update code

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['http://ecommerce-env.eba-erdm4yav.us-west-2.elasticbeanstalk.com/']
+ALLOWED_HOSTS = ['ecommerce-env.eba-erdm4yav.us-west-2.elasticbeanstalk.com/']
 
 
 # Application definition
